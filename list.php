1f4566f4d8b8dce39a6210c8a946482f8eed0a87
Navneet <navneetmails@gmail.com>
2024-07-04 10:04:06 +0530

    Create list.php

---
+++ list.php
@@ -0,0 +1,16 @@
+<?php
+$product_list = [
+    [
+        "name" => "wfTuna",
+        "link" => "https://amzn.in/d/cvrBQ4R",
+    ],
+    [
+        "name" => "wfChkn",
+        "link" => "https://amzn.in/d/7QiKuiT",
+    ],
+    [
+        "name" => "dfMeatUp",
+        "link" => "https://amzn.in/d/4KYd9nW",
+    ],
+];
+?>
